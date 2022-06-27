167c9482357b789cac057ca39cc18bd47655e020
alpha <brianmauriceoluoch@gmail.com>
2022-06-27 23:58:29 +0300

    initial commit

---
+++ chat/src/app/components/username/username.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-username',
+  templateUrl: './username.component.html',
+  styleUrls: ['./username.component.css']
+})
+export class UsernameComponent implements OnInit {
+  @Output() userNameEvent = new EventEmitter<string>();
+  userName = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  setUserName(): void {
+    this.userNameEvent.emit(this.userName);
+  }
+
+}
